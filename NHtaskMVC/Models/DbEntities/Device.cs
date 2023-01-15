ac6d6bafd37f93dc82ec7f91a31343297ca789dd
Eva Lumean <dkorcagin6@gmail.com>
2023-01-16 00:34:26 +0500

    asd

--- NHtaskMVC/Models/DbEntities/Device.cs
+++ NHtaskMVC/Models/DbEntities/Device.cs
@@ -11,7 +11,32 @@ public class Device
 
     [Column(TypeName = "varchar(20)")]
     public string? BLD { get; set; }
-
+    
     [Column(TypeName = "varchar(20)")]
     public string? UBG { get; set; }
+
+    [Column(TypeName = "varchar(20)")]
+    public string? BIL { get; set; } 
+    
+    [Column(TypeName = "varchar(20)")]
+    public string? PRO { get; set; }
+    
+    [Column(TypeName = "varchar(20)")]
+    public string? NIT { get; set; }
+    
+    [Column(TypeName = "varchar(20)")]
+    public string? KET { get; set; }
+    
+    [Column(TypeName = "varchar(20)")]
+    public string? GLU { get; set; }
+    
+    [Column(TypeName = "varchar(20)")]
+    public string? PH { get; set; }
+    
+    [Column(TypeName = "varchar(20)")]
+    public string? SG { get; set; }
+    
+    [Column(TypeName = "varchar(20)")]
+    public string? LEU { get; set; }
+    
 }
